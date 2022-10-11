d5d429de9e621040951f88789d7169a78957a964
syl <syleze777@gmail.com>
2022-10-11 12:29:35 +0200

    More documentation

--- src/core/atomicdex/json.hpp
+++ src/core/atomicdex/json.hpp
@@ -2,6 +2,8 @@
 
 namespace atomic_dex
 {
+    // Specializes this function to set the json key of a type member.
+    // Returns the json key of a type member.
     template <auto MemberPtr>
     constexpr auto json_name_of() = delete;
 }
