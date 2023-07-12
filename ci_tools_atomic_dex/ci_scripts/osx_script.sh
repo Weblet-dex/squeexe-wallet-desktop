82d54ee838263020b75bf4bb60ec28a1547bda14
Weblet <20154559+Weblet-dex@users.noreply.github.com>
2023-07-12 15:58:02 +0800

    Update osx_script.sh

--- ci_tools_atomic_dex/ci_scripts/osx_script.sh
+++ ci_tools_atomic_dex/ci_scripts/osx_script.sh
@@ -20,7 +20,7 @@ brew install autoconf \
 pip3 install yq
 export CC=clang
 export CXX=clang++
-export MACOSX_DEPLOYMENT_TARGET=10.15
+export MACOSX_DEPLOYMENT_TARGET=13.3
 
 # get curl
 #git clone https://github.com/KomodoPlatform/curl.git
@@ -39,4 +39,4 @@ sudo make -j3 install
 cd ..
 
 # get SDKs
-git clone https://github.com/KomodoPlatform/MacOSX-SDKs $HOME/sdk
+git clone https://github.com/alexey-lysiuk/macos-sdk $HOME/sdk
