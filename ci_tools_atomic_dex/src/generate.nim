d1c855bfb28176cc02fe26ae51fcd3e275e89457
Weblet-dex <shaun-007@hotmail.com>
2023-07-15 06:35:11 +0800

    no ninja

--- ci_tools_atomic_dex/src/generate.nim
+++ ci_tools_atomic_dex/src/generate.nim
@@ -13,10 +13,10 @@ proc generate_solution*(build_type: string, osx_sdk_path: string, compiler_path:
         echo "existing directory: " & full_name
     os.setCurrentDir(os.getCurrentDir().joinPath(full_name))
     assert(os.existsEnv("QT_INSTALL_CMAKE_PATH"))
-    var cmd_line = "cmake -GNinja -DCMAKE_BUILD_TYPE=" &  build_type & " " &
+    var cmd_line = "cmake -DCMAKE_BUILD_TYPE=" &  build_type & " " &
                     os.getCurrentDir().parentDir().parentDir()
     when defined(osx):
         if os.dirExists(osx_sdk_path):
-            cmd_line = cmd_line & " -DCMAKE_OSX_SYSROOT=" & osx_sdk_path & " -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15"
+            cmd_line = cmd_line & " -DCMAKE_OSX_SYSROOT=" & osx_sdk_path & " -DCMAKE_OSX_DEPLOYMENT_TARGET=12.1"
     echo "cmd line: " & cmd_line
     discard execCmd(cmd_line)
