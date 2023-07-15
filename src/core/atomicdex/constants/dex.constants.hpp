3bb9cc134fb627fb1175e805f1c11be3515a72fa
Weblet <20154559+Weblet-dex@users.noreply.github.com>
2023-07-15 10:42:40 +0800

    Touch ups

--- src/core/atomicdex/constants/dex.constants.hpp
+++ src/core/atomicdex/constants/dex.constants.hpp
@@ -8,9 +8,10 @@ namespace atomic_dex
     inline const std::string g_primary_dex_coin{DEX_PRIMARY_COIN};
     inline const std::string g_second_primary_dex_coin{DEX_SECOND_PRIMARY_COIN};
     inline const std::vector<std::string> g_default_coins{
-        "FZ45T-PLG20",
         g_primary_dex_coin,
         g_second_primary_dex_coin,
+        "FZ45T-PLG20",
+        "MATIC",
         "BTC-segwit",
         "LTC-segwit",
         "DGB-segwit",
